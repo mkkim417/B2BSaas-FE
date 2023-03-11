86a0aa3c3bf1bb64f3646c949e0e62bdcad1f480
0hyeon <djdjdjk2006@naver.com>
2023-03-11 20:00:48 +0900

    feat:부분취소 기능 활성화

--- src/page/SendMessage.tsx
+++ src/page/SendMessage.tsx
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback, useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
-import { dummyData } from '../constants/products'
 import { Pagination } from '@mantine/core'
 import * as XLSX from 'xlsx'
 interface IData {
@@ -9,31 +8,61 @@ interface IData {
   name: string
 }
 function SendMessage() {
-  const [isData, setData] = useState<any[]>()
+  const [isData, setData] = useState<any>()
   const [isKeyData, setKeyData] = useState<any>()
-  const navigate = useNavigate()
-  const onSubmit = () => {
-    alert('heelo submit')
+  const [checkedList, setCheckedList] = useState<string[]>([])
+  const [isOpen, setOpen] = useState(false)
+  const [isChecked, setIsChecked] = useState(false)
+  const fileInput = useRef<any>()
+  const [isGroupName, setGroupName] = useState('')
+  //파일초기화함수
+  const onClearAttachment = () => {
+    fileInput.current.value = ''
   }
+  //그룹이름 체인지함수
+  const groupName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setGroupName(e.target.value)
+  }
+  //선택취소함수
+  const checkedItemHandler = (value: string, isChecked: boolean) => {
+    if (isChecked) {
+      setCheckedList((prev) => [...prev, value])
+      return
+    }
+    if (!isChecked && checkedList.includes(value)) {
+      setCheckedList(checkedList.filter((item) => item !== value))
+      return
+    }
+    return
+  }
+  const checkHandler = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    value: string
+  ) => {
+    setIsChecked(!isChecked)
+    checkedItemHandler(value, e.target.checked)
+  }
+  //저장함수
+  const onSubmit = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.preventDefault()
 
-  useEffect(() => {
-    // setData(dummyData)
-  }, [])
-
+      //   console.log('checkedList:', checkedList)
+    },
+    [checkedList]
+  )
+  //엑셀읽는함수
   function readExcel(event: any) {
     let input = event.target
     let reader = new FileReader()
     reader.onload = function () {
       let data = reader.result
       let workBook = XLSX.read(data, { type: 'binary' })
-
       if (workBook.bookType !== 'xlsx' && workBook.bookType !== 'csv') {
         alert('csv, xlsx형식을 넣어주세요.')
         return
       }
-
       workBook.SheetNames.forEach(function (sheetName) {
-        console.log('SheetName: ' + sheetName)
         let rows = XLSX.utils.sheet_to_json(workBook.Sheets[sheetName])
         const jsonData = JSON.stringify(rows)
         const pareData = JSON.parse(jsonData)
@@ -44,74 +73,102 @@ function SendMessage() {
     }
     reader.readAsBinaryString(input.files[0])
   }
-  console.log(isData)
-  console.log(isKeyData)
+  console.log('csv넣은대상', isData)
+  console.log('취소된대상:', checkedList)
+  console.log(
+    '차집합 :',
+    isData && isData.filter((x: any) => !checkedList.includes(x))
+  )
   return (
     <Wrapper>
       <ContentsWrap>
         <TopContents>
-          <Input type="text" placeholder="그룹명" />
+          <Input
+            type="text"
+            placeholder="그룹명"
+            value={isGroupName}
+            onChange={groupName}
+          />
           <InputFile
             type="file"
             accept=".csv,.xlsx"
             onChange={readExcel}
+            ref={fileInput}
           ></InputFile>
-          {/* <InputFile
-            type="file"
-            accept=".csv"
-            onChange={(e: React.ChangeEvent<HTMLInputElement> | any) =>
-              console.log(e.target.files[0])
-            }
-          /> */}
         </TopContents>
-        {/* <div>
-            {isKeyData && isKeyData.map((li: any) => <span>{li}/</span>)}
-        </div> */}
         {isData && isData ? (
-          <table>
-            <thead style={{ fontWeight: 'bold' }}>
-              <tr>
-                {isKeyData &&
-                  isKeyData.map((li: any, idx: number) => (
-                    <th key={li + idx}>{li}</th>
-                  ))}
-              </tr>
-            </thead>
-            <tbody style={{ textAlign: 'center' }}>
-              {isData &&
-                isData.map((el: any, idx: number) => (
-                  <tr key={idx}>
-                    {isKeyData.map((li: any) => (
-                      <td>{el[li]}</td>
+          <MapWrapper>
+            <table>
+              <thead style={{ fontWeight: 'bold', fontSize: '13px' }}>
+                <tr>
+                  {isOpen && isOpen ? <th>선택</th> : null}
+                  {isKeyData &&
+                    isKeyData.map((li: any, idx: number) => (
+                      <th key={idx}>{li}</th>
                     ))}
-                  </tr>
-                ))}
-            </tbody>
-          </table>
+                </tr>
+              </thead>
+              <tbody style={{ textAlign: 'center' }}>
+                {isData &&
+                  isData.map((el: any, idx: number) => (
+                    <tr key={idx}>
+                      {isOpen && isOpen ? (
+                        <Td>
+                          <input
+                            type="checkbox"
+                            key={idx}
+                            checked={checkedList.includes(el)}
+                            onChange={(e) => checkHandler(e, el)}
+                          />
+                        </Td>
+                      ) : null}
+                      {isKeyData.map((li: any, idx: number) => (
+                        <Td key={idx}>{el[li]}</Td>
+                      ))}
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </MapWrapper>
         ) : (
-          // <div>
-          //   {isData.map((el: any, idx: number) => (
-          //     <div>
-          //       {isKeyData.map((li: any) => (
-          //         <span>{el[li]}</span>
-          //       ))}
-          //     </div>
-          //   ))}
-          // </div>
           //   <Pagination page={activePage} onChange={setPage} total={total} />
           <BottomContents>
             <div>생성된 고객이 없습니다. CSV 파일을 넣어주세요.</div>
           </BottomContents>
         )}
         <BtnWrap>
-          <Button onClick={() => navigate('/')}>취소</Button>
+          <Button
+            onClick={() => [
+              onClearAttachment(),
+              setData(false),
+              setGroupName(''),
+              setOpen((prev) => !prev),
+              setKeyData(''),
+              setCheckedList([]),
+            ]}
+          >
+            취소
+          </Button>
           <Button onClick={onSubmit}>저장</Button>
+          {isData && isData ? (
+            <Button onClick={() => setOpen((prev) => !prev) as any}>
+              선택삭제
+            </Button>
+          ) : null}
         </BtnWrap>
       </ContentsWrap>
     </Wrapper>
   )
 }
-
+const MapWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+const Td = styled.td`
+  font-size: 12px;
+`
 const BtnWrap = styled.div`
   display: flex;
   align-items: center;
@@ -145,17 +202,21 @@ const Wrapper = styled.div`
   gap: 30px;
   justify-content: center;
 `
-const ContentsWrap = styled.div``
+const ContentsWrap = styled.div`
+  width: 100%;
+  margin: 0 auto;
+`
 const TopContents = styled.div`
   justify-content: space-evenly;
   align-items: center;
   display: flex;
   flex-direction: column;
-  width: 100%;
+  width: 400px;
+  margin: 0 auto;
   border-radius: 15px;
   height: 100px;
   background-color: #ededed;
-  margin: 30px 0px;
+  margin: 30px auto;
 `
 
 const BottomContents = styled.div`
@@ -165,7 +226,8 @@ const BottomContents = styled.div`
   justify-content: center;
   border-radius: 15px;
   font-size: 12px;
-  width: 100%;
+  width: 600px;
+  margin: 0 auto;
   height: 250px;
   background-color: #ededed;
 `

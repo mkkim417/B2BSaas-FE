257d7d09e2fb3d32319ba31494bc1bc64c29f99e
jjolraman <jjolraman@gmail.com>
2023-03-18 11:18:50 +0900

    feat: .env 파일 추가 signup axios call 시도, 회원가입 버튼 오작동 발견 링크 추가(회원가입 페이지, 로그인 페이지)

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import axios from 'axios';
 import { useNavigate } from 'react-router';
 import { useForm, SubmitHandler } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 interface FormValues {
   email: string;
@@ -55,6 +56,7 @@ function Login() {
           />
         </StPw>
         <StLoginButton type="submit">로그인</StLoginButton>
+        <Link to="/signup">계정이 없으신가요? 여기서 회원가입 하세요</Link>
       </form>
     </Wrapper>
   );

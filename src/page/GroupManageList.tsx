91dc4ce267044513279d7ec442a08cb79b676c32
EUNBINs <dmsqls891@naver.com>
2023-04-13 13:09:27 +0900

    feat: statistic api값 연동, 그룹관리페이지 코드개선 [중간 커밋]

--- src/page/GroupManageList.tsx
+++ src/page/GroupManageList.tsx
@@ -1,6 +1,7 @@
 import axios, { AxiosError } from 'axios';
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import Pagination from 'react-js-pagination';
+import { debounce } from 'lodash';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router';
 import styled from 'styled-components';
@@ -200,8 +201,9 @@ function GroupManageList() {
   // /api/clients?index=${index}&keyword=${keyword}
   // /api/clients?index=${index}&keyword=${keyword}
   // `${process.env.REACT_APP_SERVER_URL}/api/clients?index=${currentPage}&keyword=${searchKeyword}`
+  
   // 고객리스트에서 검색호출 API
-  const getSearchData = async () => {
+  const getSearchData = (async () => {
     const response = await axios.get(
       `${process.env.REACT_APP_SERVER_URL}/api/clients?index=${currentPage}&keyword=${searchKeyword}`,
       {
@@ -212,7 +214,7 @@ function GroupManageList() {
     // console.log('검색필터 API결과', response.data.data.clients)
     setUserList(response.data.data.clients);
     return response;
-  };
+  });
 
   // 검색필터 useEffect
   useEffect(() => {
